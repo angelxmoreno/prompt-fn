89565c34ebd5af97ba797b1d36f6f0b3c9890645
Angel S. Moreno <angelxmoreno@gmail.com>
2026-01-07 13:54:59 -0500

    chore: added MD linting

---
+++ .remarkrc.mjs
@@ -0,0 +1,7 @@
+export default {
+    plugins: [
+        'remark-preset-lint-recommended', // base rules
+        ['remark-lint-maximum-line-length', false],
+        'remark-lint-no-dead-urls',
+    ],
+};
